40aa5c771aa16aed02594f82c8c78e6be7d22e1a
viniciuws <vinicius.marquesini@hotmail.com>
2018-08-26 19:08:54 -0300

    initial structure

--- src/App.vue
+++ src/App.vue
@@ -5,10 +5,5 @@
 </template>
 
 <style lang="sass">
-#app
-  font-family: 'Avenir', Helvetica, Arial, sans-serif
-  -webkit-font-smoothing: antialiased
-  -moz-osx-font-smoothing: grayscale
-  text-align: center
-  color: #2c3e50
+@import 'styles/global.sass'
 </style>
---
+++ src/app/Details/Details.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="details">
+    DETAILS
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Details',
+};
+</script>
+
+<style scoped lang="sass">
+.details
+  display: flex
+  flex-direction: column
+  justify-content: center
+  align-items: center
+</style>
