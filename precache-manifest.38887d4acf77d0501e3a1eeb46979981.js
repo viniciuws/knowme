63210ddb0d99c06d4edd74b4df43c8059495e457
Vinícius Marquesini Terzi <vinicius.marquesini@hotmail.com>
2018-12-17 20:22:36 -0200

    Updates

---
+++ precache-manifest.38887d4acf77d0501e3a1eeb46979981.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "86e9db1eba21c663ccd6ee49a146503f",
+    "url": "/img/world.86e9db1e.gif"
+  },
+  {
+    "revision": "fcf6c44dc79179815eff875ef1fcb521",
+    "url": "/static/person.json"
+  },
+  {
+    "revision": "71186abb55f17942cd6aeb8a5f975b19",
+    "url": "/img/twitter-logo.71186abb.svg"
+  },
+  {
+    "revision": "2a4d7d949e4b505e0d8c",
+    "url": "/css/app.f7ee4be2.css"
+  },
+  {
+    "revision": "a6972a6a3b33f2faf44d",
+    "url": "/css/chunk-vendors.d4819308.css"
+  },
+  {
+    "revision": "a6972a6a3b33f2faf44d",
+    "url": "/js/chunk-vendors.d68f9167.js"
+  },
+  {
+    "revision": "f1572794f96cbe22628a",
+    "url": "/js/about.d926a2f1.js"
+  },
+  {
+    "revision": "2a4d7d949e4b505e0d8c",
+    "url": "/js/app.0ac1fb3b.js"
+  },
+  {
+    "revision": "1051480692af25274aad714877af2858",
+    "url": "/img/linkedin-logo.10514806.svg"
+  },
+  {
+    "revision": "ed33174248d70251ab479ecbab33ef13",
+    "url": "/img/github-logo.ed331742.svg"
+  },
+  {
+    "revision": "5093449194ec5d1a3b3a25a7ea6d9c5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "f1572794f96cbe22628a",
+    "url": "/css/about.3af172d7.css"
+  }
+];
